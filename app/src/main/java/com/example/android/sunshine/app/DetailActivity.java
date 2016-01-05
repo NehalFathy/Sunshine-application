e64510a09f9064b8bad2f53b98187e1ab6dba820
NehalFathy <nehal.fathy101@gmail.com>
2016-01-05 16:23:31 +0200

    Lesson 3 : Location map & share button

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -3,13 +3,18 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import android.support.v7.widget.ShareActionProvider;  // takecare !!
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -53,7 +58,14 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+        private String mForecastStr;
+
         public DetailFragment() {
+
+            setHasOptionsMenu(true);
+
         }
 
         @Override
@@ -67,11 +79,51 @@ public class DetailActivity extends ActionBarActivity {
 
             //inspect the intent to get the attached data and display data
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(forecastStr);
+                        .setText(mForecastStr);
             }
             return rootView;
+        }
+
+
+        //share intent
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            //important :: prevents the activity e are sharing to from being placed onto activity stack
+            //if we don't have this flag , when click on share icon to return to app later you may end up in another app
+            //(The one that is actually handling the share intent)
+            //so it brings me to my app
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");     //to let android kno we are sharing plain text
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                        mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+
+
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                                        (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null ) {
+                               mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+
+
         }
     }
 }
